7ea0d166b3d10df9e549e5bf27c48ef71301f1b7
aescobedob <alekz@Alekzs-iMac.local>
2014-07-17 17:33:46 -0500

    Areté Motion
    Subiendo proyecto. per commit

---
+++ footer.php
@@ -0,0 +1,42 @@
+ 	 <!-- footer-2 -->
+	    <footer class="footer-2 bg-midnight-blue">
+	        <div class="container">
+	            <nav class="pull-left">
+	                <ul>
+	                    <li class="active">
+	                        <a href="#">Inicio</a>
+	                    </li>
+	                    <li>
+	                        <a href="#">Company</a>
+	                    </li>
+	                    <li>
+	                        <a href="#">Portfolio</a>
+	                    </li>
+	                    <li>
+	                        <a href="#">Blog</a>
+	                    </li>
+	                    <li>
+	                        <a href="#">Contact</a>
+	                    </li>
+	                </ul>
+	            </nav>
+	            <div class="social-btns pull-right">
+	                <a href="#"><div class="fui-vimeo"></div><div class="fui-vimeo"></div></a>
+	                <a href="#"><div class="fui-facebook"></div><div class="fui-facebook"></div></a>
+	                <a href="#"><div class="fui-twitter"></div><div class="fui-twitter"></div></a>
+	            </div>
+	            <div class="additional-links">
+	                Be sure to take a look to our <a href="#">Terms of Use</a> and <a href="#">Privacy Policy</a>
+	            </div>
+	        </div>
+	    </footer>
+
+ 	 </div> <!-- end .page-wrapper -->
+
+ <!-- Placed at the end of the document so the pages load faster -->
+        <script src="/css/common-files/js/jquery-1.10.2.min.js"></script>
+        <script src="/css/flat-ui/js/bootstrap.min.js"></script>
+        <script src="/css/common-files/js/modernizr.custom.js"></script>
+        <script src="/css/common-files/js/startup-kit.js"></script>
+        <script src="/js/script.js"></script>
+    </body>
---
+++ header.php
@@ -0,0 +1,62 @@
+
+<?php
+use google\appengine\api\users\User;
+use google\appengine\api\users\UserService;
+# Looks for current Google account session
+
+$user = UserService::getCurrentUser();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Areté Motion</title>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+        <link rel="shortcut icon" href="/css/flat-ui/images/favicon.ico">
+        <link rel="stylesheet" href="/css/flat-ui/bootstrap/css/bootstrap.css">
+        <link rel="stylesheet" href="/css/flat-ui/css/flat-ui.css">
+        <!-- Using only with Flat-UI (free)-->
+        <link rel="stylesheet" href="/css/common-files/css/icon-font.css">
+        <!-- end -->
+        <link rel="stylesheet" href="/css/style.css">
+    </head>
+
+    <body>
+
+    <div class="page-wrapper">
+            <!-- header-11 -->
+            <header class="header-11">
+                <div class="container">
+                    <div class="row">
+                        <div class="navbar col-sm-12" role="navigation">
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle"></button>
+                                <a class="brand" href="#"><img src="img/logo@2x.png" width="50" height="50" alt=""> Motion</a>
+                            </div>
+                            <div class="collapse navbar-collapse pull-right">
+                                <ul class="nav pull-left">
+                                    <li class="active"><a href="#">INICIO</a></li>
+                                    <li><a href="#">COMPANY</a></li>
+                                    <li><a href="#">PORTFOLIO</a></li>
+                                    <li><a href="#">BLOG</a></li>
+                                    <li><a href="#">CONTACT</a></li>
+                                </ul>
+                                <form class="navbar-form pull-left">
+                                    <?php if(!$user) {
+                                        ?>
+                                        <a class="btn btn-primary" href="<?php echo UserService::createLoginUrl('/') ?>">Iniciar</a>
+                                    <?
+                                    } else { ?>
+                                        <a class="btn btn-primary" href="<?php echo UserService::createLogoutUrl('/') ?>">Salir</a>
+                                    <?php } ?>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
